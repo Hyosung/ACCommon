ce0f6e57bf337b780822209152e22878f5318bc5
曉星 <sy92710xx@163.com>
2014-05-01 00:10:17 +0800

    Initial Commit

---
+++ ACCommon/ACCommon.h
@@ -0,0 +1,13 @@
+//
+//  ACCommon.h
+//  ACCommon
+//
+//  Created by 曉星 on 14-5-1.
+//  Copyright (c) 2014年 Alone Coding. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ACCommon : NSObject
+
+@end
---
+++ ACCommon/ACCommon.m
@@ -0,0 +1,13 @@
+//
+//  ACCommon.m
+//  ACCommon
+//
+//  Created by 曉星 on 14-5-1.
+//  Copyright (c) 2014年 Alone Coding. All rights reserved.
+//
+
+#import "ACCommon.h"
+
+@implementation ACCommon
+
+@end
